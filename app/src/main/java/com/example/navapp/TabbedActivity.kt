86ee52edfca71455dcb9baee462d53afb494db87
baixuan297 <chenjiayi297@gmail.com>
2025-12-23 14:01:47 +0100

    TabLayout

--- app/src/main/java/com/example/navapp/TabbedActivity.kt
+++ app/src/main/java/com/example/navapp/TabbedActivity.kt
@@ -1,20 +1,15 @@
 package com.example.navapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.navapp.R
 
 class TabbedActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_tabbed)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 设置当前 Activity 的布局文件
+        setContentView(R.layout.activity_tabbed)
+    }
 }
