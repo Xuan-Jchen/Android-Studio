2d7088a077f1b88bd745538310284eca59b076a0
Xuan-Jchen <147503159+Xuan-Jchen@users.noreply.github.com>
2025-12-23 14:07:40 +0100

    NavApp

---
+++ NavApp/app/src/main/java/com/example/navapp/Drawer2Fragment.kt
@@ -0,0 +1,34 @@
+package com.example.navapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class Drawer2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate fragment layout
+        return inflater.inflate(R.layout.fragment_drawer2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Get views
+        val felizNavidadText = view.findViewById<TextView>(R.id.felizNavidad)
+        val button = view.findViewById<Button>(R.id.drawer2Button)
+
+        // Button click listener
+        button.setOnClickListener {
+            felizNavidadText.text = "feliz navidad"
+        }
+    }
+}
